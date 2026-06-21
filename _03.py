6ef7b47c0b4f701ee8b537acd0b541003f5516bf
tajeshnishad <tn4483@srmist.edu.in>
2026-06-21 18:12:46 +0530

    day 03 - Getting output of addittion(+),subtraction(-),multi(*),divi(/) using variables

---
+++ _03.py
@@ -0,0 +1,11 @@
+""" Getting output of addittion(+),subtraction(-),multi(*),divi(/)  """
+
+
+print("SUNNY","suii" , 7)
+print("""HELLO WORLD""")
+print(5)
+print(8+7)
+print(2*4)
+print(7-2)
+print(3/2)
+print(4*3)
