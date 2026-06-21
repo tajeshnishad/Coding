4ac8016d4c61b09fc834cd05e90d65434fb3b02a
tajeshnishad <tn4483@srmist.edu.in>
2026-06-22 00:38:51 +0530

    Importing libraries and Using Variables

--- _02.py
+++ _02.py
@@ -1,4 +1,8 @@
 """importing Lib."""
 print("SUNNY")
-import hashlib
-print ("goo")
+import hashlib ; print ("hello intrepeter!")
+message="My self Sunny";
+print(message)
+
+message='i am self obbssesed ';              '''HERE I HAVE OVERWRITTEN THE PREVIOUS MESSAGE WITH THAT OF NEW ONE '''
+print(message)
