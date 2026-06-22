a51f2343133fd8b574bc519a63c399fdcf772135
tajeshnishad <tn4483@srmist.edu.in>
2026-06-22 08:49:38 +0530

    Exercise-02

---
+++ Exercise2.py
@@ -0,0 +1,5 @@
+'''assign a message to a variable , and print that message. Then change the value of the variable to a new message'''
+a='message'
+print(a)
+a='i m overwritting a message command'
+print(a)
