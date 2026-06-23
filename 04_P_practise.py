232cddb63e6954e26963ed73e09999262aa58762
tajeshnishad <tn4483@srmist.edu.in>
2026-06-23 11:30:42 +0530

    day-4 Previous Sessions Practise

---
+++ 04_P_practise.py
@@ -0,0 +1,35 @@
+print("sunny")
+print(9*2.2)
+print("hey I m a bad boiiii")
+
+#--------->#hey sunny these is my intro (comment)
+
+
+print("i m a defence aspirant \ndone my coaching from PRAHAR defence acadamy \nnoice ")
+# print("hello world")
+
+
+print(9*2 ,  "Y is my sisi", "X is my bhai ")  #my intro to family
+
+"""       '''multiple line comments 
+In these lecture i have studied about ######comments###### 
+and ####### multipleline comments #########'''       """
+
+""" i have also studied about \n (whcih takes the statemet to the next line) """
+
+
+
+print('i m a good boii "my name is sunny" ')
+'''|
+   |
+   |
+   |
+   |
+   | (best example to remember )'''
+
+
+
+"""MY SELF"""
+print("my name is Sunny \n my fathers has a very good personality \n my has it too  " 
+      '\n i have always wanted to have a pet names "(simbha)"'
+      '\n i have completed my schooling from various school in both private and Govt. both ' )
