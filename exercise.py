abb2cc7608da343733181b6ff7164300f8d8fa10
tajeshnishad <tn4483@srmist.edu.in>
2026-06-28 13:08:47 +0530

    Previous exercises  *--- *

---
+++ exercise.py
@@ -0,0 +1,4 @@
+f_name='eric'
+message=f'"Hello {f_name.title()},would you like to learn some Python today?"';
+print(message);
+
---
+++ exercise__.py
@@ -0,0 +1,6 @@
+f_name='tajeSh'
+l_name='NISHad'
+full_name=f'{f_name} {l_name}'
+print(full_name.lower());
+print(full_name.upper());
+print(full_name.title());
