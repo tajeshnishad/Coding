9cbcf81dfef57dc956f0366a6c60b62dede636ef
tajeshnishad <tn4483@srmist.edu.in>
2026-06-29 08:19:03 +0530

    Day 08 List shorting(tem. & perm.)

---
+++ 08.py
@@ -0,0 +1,17 @@
+cars=['bmw','mahindra','tata','audi']
+print(f'{cars},ORIGINAL')
+
+print(f'{sorted(cars)},temperorly sorted')
+
+print(f'{cars}see,ORIGINAL is printed here again!')
+
+cars.reverse()
+print(f'{cars},printed original in reverse')
+
+cars.sort()
+print(f'{cars},permenantly sorted in alphabetical order')
+
+cars.sort(reverse=True)
+print(f'{cars},permanently sorted in reverse order')
+
+print(f'{cars},Lets see !,how is the cars list is arranged over here at fianl step')
